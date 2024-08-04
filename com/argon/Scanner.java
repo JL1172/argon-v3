d33bd5280ca9ab98d7ee5da60fe3035518294d42
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-08-04 00:16:22 -0400

    JL ArgonV3 added more methods to scanner class

---
+++ com/argon/Scanner.java
@@ -0,0 +1,57 @@
+package com.argon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Scanner {
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 0;
+
+    Scanner(String source) {
+        this.source = source;
+    } 
+    
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            this.start = this.current;
+            scanToken();
+        }
+        this.tokens.add(new Token(TokenType.EOF, "", null, this.line));
+        return this.tokens;
+    }
+
+    private boolean isAtEnd() {
+        return this.current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch(c) {
+            case '(' : addToken(TokenType.LEFT_PAREN); break;
+            case ')' : addToken(TokenType.RIGHT_PAREN); break;
+            case '{' : addToken(TokenType.LEFT_BRACE); break;   
+            case '}' : addToken(TokenType.RIGHT_BRACE); break;   
+            case ',' : addToken(TokenType.COMMA); break;   
+            case '.' : addToken(TokenType.DOT); break;   
+            case '-' : addToken(TokenType.MINUS); break;   
+            case '+' : addToken(TokenType.PLUS); break;   
+            case ';' : addToken(TokenType.SEMICOLON); break;   
+            case '*' : addToken(TokenType.STAR); break;   
+        }
+    }
+
+    private char advance() {
+        return this.source.charAt(current++);
+    }
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start,current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}

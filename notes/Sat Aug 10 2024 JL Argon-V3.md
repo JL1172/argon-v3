ab0cf5949ced0b1ee5d44d339e02489d3c0f47ad
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-08-10 15:03:22 -0400

    -e JL ArgonV3 2024-08-10T15:03:49-04:00
    -e JL ArgonV3 2024-08-10T14:45:53-04:00
    
    this is test no 2 for the pre commit hook

--- notes/Sat Aug 10 2024 JL Argon-V3.md
+++ notes/Sat Aug 10 2024 JL Argon-V3.md
@@ -1,4 +1,6 @@
 > [!IMPORTANT] This Note Gives Details On Where I Left Off
  # Sat Aug 10 2024 JL ArgonV3 
  # Description: 
- this is test no 2 for the pre commit hook
+ -e JL ArgonV3 2024-08-10T14:45:53-04:00 
+
+this is test no 2 for the pre commit hook
--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,14 @@
 import * as fs from "fs";
 import * as readline from "readline-sync";
+import { exec } from "child_process";
 
 async function NOTE_SCRIPT() {
   try {
     const answer = readline.keyInYN(
       "Is This Your Final Commit Of The Day? - if (YES) (THEN) script will write a file with progress notes"
     );
-    if (answer === true || answer) { //handles y or n bool || string cases
+    if (answer === true || answer) {
+      //handles y or n bool || string cases
       const path = ".git/COMMIT_EDITMSG";
       const commit_message = await new Promise((resolve, reject) => {
         fs.readFile(path, (error, data) => {
@@ -19,8 +21,37 @@ async function NOTE_SCRIPT() {
       });
       const time_stamp = new Date().toDateString();
       const formatted_note = `> [!IMPORTANT] This Note Gives Details On Where I Left Off\n # ${time_stamp} JL ArgonV3 \n # Description: \n ${commit_message}`;
-      const file_name = time_stamp.concat(" JL Argon-V3");
+      const file_name = time_stamp.concat(" JL Argon-V3.md");
       fs.writeFileSync(`notes/${file_name}`, formatted_note);
+      const command_one_stdout = await new Promise((resolve, reject) => {
+        exec("git add .", (error, stdout) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(stdout);
+          }
+        });
+      });
+      console.log(`COMMAND ONE STDOUT: ${command_one_stdout}`);
+      const past_commit_message = await new Promise((resolve, reject) => {
+        fs.readFile(path, (error, data) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+      });
+      const command_two_stdout = await new Promise((resolve, reject) => {
+        exec(`git commit -m "${past_commit_message}"`, (error, stdout) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(stdout);
+          }
+        });
+      });
+      console.log(`COMMAND TWO STDOUT: ${command_two_stdout}`);
     } else {
       console.log("Proceeding with commit.");
     }

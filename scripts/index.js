7d5dc3ce5615e1b636606d16992f16b6570ac7f3
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-08-10 17:13:50 -0400

    -e JL ArgonV3 2024-08-10T17:13:50-04:00
    final commit of pre-commit test

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,31 @@
 import * as fs from "fs";
 import * as readline from "readline-sync";
-import { exec, execSync } from "child_process";
+import { exec } from "child_process";
 
 async function NOTE_SCRIPT() {
   try {
     const path = ".git/COMMIT_EDITMSG";
-    const answer = readline.keyInYN(
-      "Is This Your Final Commit Of The Day? - if (YES) (THEN) script will write a file with progress notes"
-    );
-    if (answer === true || answer) {
-      //handles y or n bool || string cases
-      const commit_message = await new Promise((resolve, reject) => {
-        fs.readFile(path, { encoding: "utf-8" }, (error, data) => {
-          if (error) {
-            reject("Read Commit Message Error no2: \n" + error);
-          } else {
-            resolve(data);
-          }
-        });
+    //handles y or n bool || string cases
+    const commit_message = await new Promise((resolve, reject) => {
+      fs.readFile(path, { encoding: "utf-8" }, (error, data) => {
+        if (error) {
+          reject("Read Commit Message Error no1: \n" + error);
+        } else {
+          resolve(data);
+        }
       });
+    });
+    const last_four_chars = commit_message.split(" ").at(-1) === "---f";
+    if (last_four_chars === true) {
+      console.log("Proceeding automated commit ammendment.");
+    } else {
       const time_stamp = new Date().toDateString();
-      const formatted_note = `> [!IMPORTANT] This Note Gives Details On Where I Left Off\n\n # ${time_stamp} JL ArgonV3 \n\n # Description: \n\n ${commit_message}`;
+      const formatted_note = `> [!IMPORTANT] Notes For ${time_stamp} JL ArgonV3 \n\n # Description: \n\n ${commit_message}`;
       const file_name = time_stamp.concat(" JL Argon-V3.md");
-      fs.writeFileSync(`notes/${file_name}`, formatted_note);
+
+      fs.writeFile(`notes/${file_name}`, formatted_note, "utf-8", (error) => {
+        throw new Error("Write File Error: " + error);
+      });
 
       const command_one_stdout = await new Promise((resolve, reject) => {
         exec("git add .", (error, stdout) => {
@@ -33,16 +36,31 @@ async function NOTE_SCRIPT() {
           }
         });
       });
-      console.log(`COMMAND ONE STDOUT: ${command_one_stdout}`);
-      exec(`git commit --amend`, (error, stdout) => {
-        if (error) {
-          throw new Error("Git Commit --amend Error: \n" + error);
-        } else {
-          console.log(`COMMAND TWO STDOUT: ${stdout}`);
-        }
+      console.log(
+        `COMMAND ONE STDOUT [git add . command result]: ${command_one_stdout}`
+      );
+
+      const recent_msg = await new Promise((resolve, reject) => {
+        fs.readFile(path, { encoding: "utf-8" }, (error, data) => {
+          if (error) {
+            reject("Read Commit Message Error no2: \n" + error);
+          } else {
+            resolve(data);
+          }
+        });
       });
-    } else {
-      console.log("Proceeding with commit (b).");
+      const command_two_stdout = await new Promise((resolve, reject) => {
+        exec(`git commit -m ${recent_msg} ---f`, (error, stdout) => {
+          if (error) {
+            throw new Error("Git Commit -m Error [custom flag]: \n" + error);
+          } else {
+            console.log(`COMMAND TWO STDOUT: ${stdout}`);
+          }
+        });
+      });
+      console.log(
+        `COMMAND TWO STDOUT [git commit -m ---f command result]: ${command_two_stdout}`
+      );
     }
   } catch (err) {
     console.error(

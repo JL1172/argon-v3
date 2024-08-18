839d3088d3a107379629aa3df0a0fbaeb55c18e9
Jacob Lang <130790079+JL1172@users.noreply.github.com>
2024-08-17 20:22:10 -0400

    -e JL ArgonV3 2024-08-17T20:22:12-04:00
    finished writing script that generates AST classes. this is all confusing meta boiler plate code that i need to further research.
     ---f

--- com/tool/GenerateAst.java
+++ com/tool/GenerateAst.java
@@ -35,4 +35,19 @@ public class GenerateAst {
         writer.println("}");
         writer.close();
     }
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("  static class " + className + " extends " + baseName + " {"); 
+        writer.println("    " + className + "(" + fieldList + ") {");
+        String[] fields = fieldList.split(", ");
+        for (String field: fields) {
+            String name = field.split(" ")[1];
+            writer.println("     this." + name + " = " + name + ";");
+        }
+        writer.println("    }");
+        writer.println();
+        for (String field: fields) {
+            writer.println("    final " + field + ";");
+        }
+        writer.println("  }");
+    }
 }
---
+++ notes/Sat Aug 17 2024 JL Argon-V3.md
@@ -0,0 +1,6 @@
+> [!IMPORTANT] 
+ > These Notes Are For User JL 
+
+ # Description: 
+
+ finished writing script that generates AST classes. this is all confusing meta boiler plate code that i need to further research.
